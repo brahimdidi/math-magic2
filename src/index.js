205325a357483b86023bf10bceff0d961a4b6d31
brahim <brahimgb1team@gmail.com>
2022-06-10 10:21:13 +0000

    add routers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <App />
+    </Router>
   </React.StrictMode>,
 );
