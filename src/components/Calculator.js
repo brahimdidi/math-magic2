b30ccbe90769eeb84ca953e335c23c87fcbfa488
brahim <brahimgb1team@gmail.com>
2022-06-10 10:22:18 +0000

    add title section to match design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import './Calculator.css';
 
 const Calculator = () => {
   const [state, setState] = useState({ total: null, next: null, operation: null });
@@ -10,34 +11,37 @@ const Calculator = () => {
 
   const { total, operation, next } = state;
   return (
-    <div className="calculator">
-      <div className="input">
-        {total}
-        {operation}
-        {next}
+    <section className="calculator-section">
+      <div className="title">Let&#39;s do some Math!</div>
+      <div className="calculator">
+        <div className="input">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="calculator-buttons">
+          <button onClick={handler} type="button">AC</button>
+          <button onClick={handler} type="button">+/-</button>
+          <button onClick={handler} type="button">%</button>
+          <button onClick={handler} className="bg-orange" type="button">÷</button>
+          <button onClick={handler} type="button">7</button>
+          <button onClick={handler} type="button">8</button>
+          <button onClick={handler} type="button">9</button>
+          <button onClick={handler} className="bg-orange button-x" type="button">x</button>
+          <button onClick={handler} type="button">4</button>
+          <button onClick={handler} type="button">5</button>
+          <button onClick={handler} type="button">6</button>
+          <button onClick={handler} className="bg-orange" type="button">-</button>
+          <button onClick={handler} type="button">1</button>
+          <button onClick={handler} type="button">2</button>
+          <button onClick={handler} type="button">3</button>
+          <button onClick={handler} className="bg-orange" type="button">+</button>
+          <button onClick={handler} className="button-0" type="button">0</button>
+          <button onClick={handler} type="button">.</button>
+          <button onClick={handler} className="bg-orange" type="button">=</button>
+        </div>
       </div>
-      <div className="calculator-buttons">
-        <button onClick={handler} type="button">AC</button>
-        <button onClick={handler} type="button">+/-</button>
-        <button onClick={handler} type="button">%</button>
-        <button onClick={handler} className="bg-orange" type="button">÷</button>
-        <button onClick={handler} type="button">7</button>
-        <button onClick={handler} type="button">8</button>
-        <button onClick={handler} type="button">9</button>
-        <button onClick={handler} className="bg-orange button-x" type="button">x</button>
-        <button onClick={handler} type="button">4</button>
-        <button onClick={handler} type="button">5</button>
-        <button onClick={handler} type="button">6</button>
-        <button onClick={handler} className="bg-orange" type="button">-</button>
-        <button onClick={handler} type="button">1</button>
-        <button onClick={handler} type="button">2</button>
-        <button onClick={handler} type="button">3</button>
-        <button onClick={handler} className="bg-orange" type="button">+</button>
-        <button onClick={handler} className="button-0" type="button">0</button>
-        <button onClick={handler} type="button">.</button>
-        <button onClick={handler} className="bg-orange" type="button">=</button>
-      </div>
-    </div>
+    </section>
   );
 };
 
